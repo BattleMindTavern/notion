d1f854e999d4e963ae88cae2a3114db46526d17f
BattleMindTavern <94011191+BattleMindTavern@users.noreply.github.com>
2021-11-09 22:25:54 +0100

    Create widget_mondialrelay.js

---
+++ widget_mondialrelay.js
@@ -0,0 +1,11 @@
+!function(){
+	// Parameterized the widget
+  $(document).ready(function () {  
+    $("#Zone_Widget").MR_ParcelShopPicker({  
+      Target: "#ParcelShopCode",
+      Brand: "CC21LZE5",
+      Country: "FR",
+      NbResults:"15"
+    });  
+  });
+}();
